c71bafe3deb2b4d9419aba753f39dbf7c7ba7a16
asfalcao <45610989+asfalcao@users.noreply.github.com>
2018-12-19 18:58:20 -0200

    Add files via upload

---
+++ config/database.php
@@ -0,0 +1,30 @@
+<?php
+
+// 3.5 Connect to database - Created
+
+class Database{
+ 
+    // specify your own database credentials
+    private $host = "localhost";
+    private $db_name = "api_db";
+    private $username = "root";
+    private $password = "Celepar";
+    public $conn;
+ 
+    // get the database connection
+    public function getConnection(){
+ 
+        $this->conn = null;
+ 
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        }catch(PDOException $exception){
+            echo "Connection error: " . $exception->getMessage();
+        }
+ 
+        return $this->conn;
+    }
+}
+
+?>
